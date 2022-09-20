ca2755d555c15156c25495d3d822efe6d03d5bac
Pavanarathod <edrocullen13@gmail.com>
2022-09-20 11:42:02 +0530

    adding date form

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const dateForm = document.querySelector(".date_form");
+const dateInput = document.querySelectorAll(".date__input");
+
+dateInput.forEach((date) => {
+  date.addEventListener("change", (e) => {
+    alert(`No Booking available to this date ${dateInput.value}`);
+  });
+});
